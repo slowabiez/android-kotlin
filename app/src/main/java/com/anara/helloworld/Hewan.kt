ce2c6f6cd51cb0e05b45aa0dbf39e4aa78fd1806
Andreas Saputra <slow.abiez@gmail.com>
2022-08-31 09:33:09 +0700

    Menambahkan data hewan

---
+++ app/src/main/java/com/anara/helloworld/Hewan.kt
@@ -0,0 +1,6 @@
+package com.anara.helloworld
+
+data class Hewan(
+    val nama: String,
+    val gambarResId: Int
+)
--- app/src/main/java/com/anara/helloworld/MainActivity.kt
+++ app/src/main/java/com/anara/helloworld/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = arrayListOf<Hewan>().apply {
+            add(Hewan("Ayam", R.drawable.ayam))
+            add(Hewan("Bebek", R.drawable.bebek))
+            add(Hewan("Domba", R.drawable.domba))
+            add(Hewan("Kambing", R.drawable.kambing))
+            add(Hewan("Sapi", R.drawable.sapi))
+        }
     }
 }
