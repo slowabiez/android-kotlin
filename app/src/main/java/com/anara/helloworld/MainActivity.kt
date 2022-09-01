32cc2d847be8f0ad21c471aa55a45326ee4dd6a6
Andreas Saputra <slow.abiez@gmail.com>
2022-09-01 08:26:23 +0700

    - Membuat layout - Memasangkan aksi tombol - Menambahkan perhitungan - Melakukan sanity check

--- app/src/main/java/com/anara/helloworld/MainActivity.kt
+++ app/src/main/java/com/anara/helloworld/MainActivity.kt
@@ -2,10 +2,60 @@ package com.anara.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.anara.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        this.binding.etQty.setText("1")
+
+        this.binding.btnMin.setOnClickListener { this.updateQty(-1) }
+        this.binding.btnPlus.setOnClickListener { this.updateQty(1) }
+
+        this.binding.btnOrder.setOnClickListener { this.placeOrder() }
+    }
+
+    private fun updateQty(amount: Int) {
+        val qty = this.binding.etQty.text.toString()
+        var total = amount
+        if (qty.isNotEmpty()) total += qty.toInt()
+        if (total < 1) total = 1
+        this.binding.etQty.setText("$total")
+    }
+
+    private fun placeOrder() {
+        val name = this.binding.etName.text
+        if (name.isEmpty()) return Toast.makeText(
+            this,
+            getString(R.string.name_required),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        val qty = this.binding.etQty.text.toString()
+        if (qty.isEmpty()) return Toast.makeText(
+            this,
+            getString(R.string.qty_required),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        var price = 10000
+        val cream = 5000
+        val chocolate = 7500
+
+        if (this.binding.cbCream.isChecked) price += cream
+        if (this.binding.cbChocolate.isChecked) price += chocolate
+
+        price *= qty.toInt()
+
+        val result = getString(R.string.result, name, price)
+
+        this.binding.tvResult.text = result
     }
 }
