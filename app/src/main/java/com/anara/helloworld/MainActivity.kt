833347a2874070e686c32e6a109b30a1a778cb94
Andreas Saputra <slow.abiez@gmail.com>
2022-09-01 09:21:18 +0700

    Membuat layout untuk list

--- app/src/main/java/com/anara/helloworld/MainActivity.kt
+++ app/src/main/java/com/anara/helloworld/MainActivity.kt
@@ -2,15 +2,8 @@ package com.anara.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var imageView: ImageView
-    private lateinit var textView: TextView
-    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +16,5 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Kambing", R.drawable.kambing))
             add(Hewan("Sapi", R.drawable.sapi))
         }
-
-        imageView = findViewById(R.id.imageView)
-        textView = findViewById(R.id.textView)
-
-        showData(data[index])
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            index = (index + 1) % data.size
-            showData(data[index])
-        }
-    }
-
-    private fun showData(hewan: Hewan) {
-        imageView.setImageResource(hewan.gambarResId)
-        textView.text = hewan.nama
     }
 }
